a8596efcd96fa85a2922e4472ccddac6ea8277fa
t4ulquiorra <t4.ulquiorra@email.com>
2026-04-14 13:08:04 +0530

    Fix: use WEB_REMIX client for player requests

--- innertube/src/main/kotlin/com/ecoute/innertube/models/bodies/PlayerBody.kt
+++ innertube/src/main/kotlin/com/ecoute/innertube/models/bodies/PlayerBody.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class PlayerBody(
-    val context: Context = Context.DefaultAndroid,
+    val context: Context = Context.DefaultWeb,
     val videoId: String,
     val playlistId: String? = null
 )
