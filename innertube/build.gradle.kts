47d0cdb216dfa4b2cffe05a15fd852b402324483
t4ulquiorra <t4.ulquiorra@email.com>
2026-04-15 14:48:00 +0530

    Fix ktor client alias: okhttp -> cio

--- innertube/build.gradle.kts
+++ innertube/build.gradle.kts
@@ -24,7 +24,7 @@ kotlin {
 
 dependencies {
     implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.cio)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.encoding)
     implementation(libs.ktor.serialization.json)
